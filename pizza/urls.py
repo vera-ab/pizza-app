aa7c1a6399fb6f3181fd128807f2a0d718ca5385
vera-ab <verunja108@gmail.com>
2023-04-17 10:12:45 +0300

    Added view for create custom pizza and new url for it

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import index, PizzaList
+from .views import index, PizzaList, CustomCreateView
 
 app_name = 'pizza'
 
 urlpatterns = [
     #path("admin/", admin.site.urls),
     path("", PizzaList.as_view(), name="pizza-list"),
+    path('custom-pizza/', CustomCreateView.as_view(), name="custom-pizza"),
     #path("", include("products.urls", namespace="products")),
     #path('accounts/', include(('django.contrib.auth.urls', 'accounts'), namespace="accounts")),
     #path('auth/', include('authentication.urls', namespace="auth")),
--- pizza/views.py
+++ pizza/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
 from .models import Pizza, Category, Ingredient, Order
@@ -19,3 +20,18 @@ class PizzaList(TemplateView):
         context = super().get_context_data(**kwargs)
         pizzas = Pizza.objects.all()
         return {'pizzas': pizzas}
+
+
+class CustomCreateView(CreateView):
+    model = Pizza
+    fields = '__all__'
+    template_name = 'custom-pizza.html'
+
+    def form_valid(self, form):
+        product = form.save(commit=False)
+        product.save()
+        form.save_m2m()
+        return redirect('/')
+
+    def get_success_url(self):
+        return reverse_lazy('/')
